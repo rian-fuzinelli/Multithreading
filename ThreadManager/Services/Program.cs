8ad9e42482b3b4905bb9eea6f76523eda7a7630a
rian-fuzinelli <riancaiofuzinelli12@gmail.com>
2025-05-02 19:25:01 -0300

    feat: adding classes to multithreading studies

---
+++ ThreadManager/Services/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ThreadManager.Services;
+
+public class Program
+{
+    private static readonly object lockObject = new object();
+    static void ThreadManager(int id)
+    {
+        lock (lockObject)
+        {
+            Console.WriteLine($"Thread {id} has started");
+        }
+
+        for(int i = 1; i <= 5; i++)
+        {
+            lock (lockObject)
+            {
+                Console.WriteLine($"Thread {id} executing the step {i}");
+            } 
+            Thread.Sleep(1000);
+        }
+        
+        lock (lockObject)
+        {
+            Console.WriteLine($"Thread {id} has ended");
+        }
+    }
+    static void Main(string[] args)
+    {
+        Thread[] threads = new Thread[3];
+
+        for (int i = 0; i < threads.Length; i++)
+        {
+            threads[i] = new Thread((() => ThreadManager(i + 1)));
+            
+            threads[i].Start();
+        }
+        
+        foreach(Thread t in threads)
+        {
+            t.Join();
+        }
+        
+        Console.WriteLine("All threads are finished");
+    }
+}
---
+++ ThreadManager/Services/ThreadLock.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadManager.Services;
+
+public static class ThreadLock
+{
+    private static readonly object lockObject = new object();
+    
+    static void ThreadTask(int id)
+    {
+        lock (lockObject)
+        {
+            Console.WriteLine($"Thread {id} has started");
+        }
+
+        for (int i = 1; i <= 3; i++)
+        {
+            lock (lockObject)
+            {
+                Console.WriteLine($"Thread {id} executing the step {i}");
+            }
+            Thread.Sleep(1000);
+        }
+
+        lock (lockObject)
+        {
+            Console.WriteLine($"Thread {id} has ended");
+        }
+    }
+
+    public static void ThreadCount(string[] args)
+    {
+        int threadsSize = 3;
+
+        Thread[] threads = new Thread[threadsSize];
+
+        for (int i = 0; i < threadsSize; i++)
+        {
+            int id = i + 1;
+            threads[i] = new Thread(() => ThreadTask(id));
+        }
+
+        foreach (Thread t in threads)
+        {
+            t.Join();
+        }
+        
+        Console.WriteLine("All threads are finished");
+    }
+    
+}
